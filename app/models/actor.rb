056b434b57441202bd7d4d4e0d3832e8f8fd5811
Brian Nicholls <nichol88@gmail.com>
2020-01-24 18:03:28 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,10 @@ class Actor < ActiveRecord::Base
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  def list_roles
+    self.characters.map do |c|
+      c.name
+    end
+  end
 end
