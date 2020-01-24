67a2c631f31be74ba65178363d2d350ade727235
Brian Nicholls <nichol88@gmail.com>
2020-01-24 18:20:33 +0000

    Automatically backed up by Learn

--- db/migrate/20200124181815_add_data_to_shows.rb
+++ db/migrate/20200124181815_add_data_to_shows.rb
@@ -3,6 +3,5 @@ class AddDataToShows < ActiveRecord::Migration[5.2]
     add_column :shows, :day :string
     add_column :shows, :genre :string
     add_column :shows, :season :string
-    end
   end
 end
