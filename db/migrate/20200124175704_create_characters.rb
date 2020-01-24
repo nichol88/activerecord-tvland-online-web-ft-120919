aa7cccf23323144dfae398a097581faaf584f0be
Brian Nicholls <nichol88@gmail.com>
2020-01-24 17:57:40 +0000

    Automatically backed up by Learn

--- db/migrate/20200124175704_create_characters.rb
+++ db/migrate/20200124175704_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :actors do |t|
       t.string :name
-      t.string :last_name
+      t.integer :actor_id
     end
   end
 end
